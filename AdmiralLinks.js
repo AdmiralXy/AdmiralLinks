85260a9ec79aa88fb24865a54e52fe36e4a7c5f2
Danil Makin <65929505+AdmiralXy@users.noreply.github.com>
2020-08-21 21:14:08 +0300

    Update AdmiralLinks.js

--- AdmiralLinks.js
+++ AdmiralLinks.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     Array.prototype.forEach.call(document.querySelectorAll('[data-uri]'), function(element) {
         element.onclick=function() {
-            (this.dataset.target == '_blank') ? window.open(this.dataset.uri, '_blank') : window.location.href = this.dataset.uri;
+            if (this.dataset.target == '_window')
+                window.open(this.dataset.uri,'targetWindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=500px,height=500px');
+            else if (this.dataset.target == '_blank')
+                window.open(this.dataset.uri, '_blank');
+            else
+                window.location.href = this.dataset.uri
         }
     });
 });
